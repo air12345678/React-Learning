f6f6c7ca58fc7cfe189734dd6bb336bbe60a40ef
Kaushal Agrawal <kaushal.agrawal@nagarro.com>
2021-06-26 13:55:21 +0530

    Update Commit

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,12 +6,12 @@ import classes from './Home.module.css';
 import AuthContext from '../../store/auth-context';
 
 const Home = (props) => {
-  const authCtx = useContext(AuthContext);
+  const ctx = useContext(AuthContext);
 
   return (
     <Card className={classes.home}>
       <h1>Welcome back!</h1>
-      <Button onClick={authCtx.onLogout}>Logout</Button>
+      <Button onClick={ctx.onLogout}>Logout</Button>
     </Card>
   );
 };
