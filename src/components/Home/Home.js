eb4550267cdf3808718e4b5bba686fcd60a25444
Kaushal Agrawal <kaushal.agrawal@nagarro.com>
2021-06-30 21:55:59 +0530

    Latest Commit

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -10,7 +10,7 @@ const Home = (props) => {
 
   return (
     <Card className={classes.home}>
-      <h1>Welcome Back!!!</h1>
+      <h1>Welcome Back😃</h1>
       <Button onClick={context.onLogout}>Logout</Button>
     </Card>
   );
